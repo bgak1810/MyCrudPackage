7f56ef766328ecf62ecca4688a76e055f4da86da
adarsh.k <adarsh.k@DWPGTS-LP-064>
2022-09-20 17:09:44 +0530

    first commit

---
+++ CRUD.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCrudPackage
+{
+    public class CRUD
+    {
+
+        public void Create()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+        public void Read()
+        {
+
+        }
+        public void Delete()
+        {
+
+        }
+    }
+}
