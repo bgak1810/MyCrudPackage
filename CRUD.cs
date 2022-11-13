f54e3706694fd28d0df1cefce76829ee28f1b3cb
adarsh.k <adarsh.k@DWPGTS-LP-064>
2022-11-13 11:46:44 +0530

    readme file updated with additional crud method

--- CRUD.cs
+++ CRUD.cs
@@ -1,29 +1,238 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace MyCrudPackage
 {
-    public class CRUD
+    public static class Crud
     {
-
-        public void Create()
+        /// <summary>
+        /// myobj - being the model of your object
+        /// spname - provide your stored procedure name
+        /// sqlconnection - provide your complete connection string 
+        /// This can be used to do the transactions such as Insertion,Deletion,Updation
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="myobj"></param>
+        /// <param name="spname"></param>
+        /// <param name="sqlconnection"></param>
+        /// <returns>the value you send it from stored procedure</returns>
+        public static object DBTransaction<T>(T myobj, string spname, string sqlconnection)
         {
+            object myreturnobject = null;
+
+            using (SqlConnection con = new SqlConnection(sqlconnection))
+            {
+                using (SqlCommand sqlcmd = new SqlCommand(spname, con))
+                {
+                    var properties = typeof(T).GetProperties();
+                    foreach (var property in properties)
+                    {
+                        SqlParameter sqlparam = new SqlParameter("@" + property.Name, property.GetValue(myobj, null));
+                        sqlcmd.Parameters.Add(sqlparam);
+                    }
+
+                    con.Open();
+                    sqlcmd.CommandType = CommandType.StoredProcedure;
+                    myreturnobject = sqlcmd.ExecuteScalar();
+                    con.Close();
+                }
+            }
+
+            return myreturnobject;
+        }
+
+        /// <summary>
+        /// myobj - being the model of your object
+        /// spname - provide your stored procedure name
+        /// sqlconnection - provide your complete connection string 
+        /// This will return multiple or single dataset(s)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="myobj"></param>
+        /// <param name="spname"></param>
+        /// <param name="sqlconnection"></param>
+        /// <returns>the value you send it from stored procedure</returns>
+        public static DataSet GetDataSet<T>(T myobj, string spname, string sqlconnection)
+        {
+            DataSet ds = new DataSet();
+            using (SqlConnection con = new SqlConnection(sqlconnection))
+            {
+
+                using (SqlCommand sqlcmd = new SqlCommand(spname, con))
+                {
+                    var properties = typeof(T).GetProperties();
+                    foreach (var property in properties)
+                    {
+                        SqlParameter sqlparam = new SqlParameter("@" + property.Name, property.GetValue(myobj, null));
+                        sqlcmd.Parameters.Add(sqlparam);
+                    }
+
+                    sqlcmd.Connection = con;
+                    con.Open();
+                    sqlcmd.CommandType = CommandType.StoredProcedure;
+                    using (SqlDataAdapter sda = new SqlDataAdapter(sqlcmd))
+                    {
+
+                        sda.Fill(ds);
+                    }
+                    con.Close();
+                }
+            }
+            return ds;
+        }
+
+        /// <summary>
+       
+        /// spname - provide your stored procedure name
+        /// sqlconnection - provide your complete connection string 
+        /// This will return multiple or single dataset(s)
+        /// </summary>
+      
+        /// <param name="spname"></param>
+        /// <param name="sqlconnection"></param>
+        /// <returns>the value you send it from stored procedure</returns>
+        public static DataSet GetDataSet(string spname, string sqlconnection)
+        {
+            DataSet ds = new DataSet();
+            using (SqlConnection con = new SqlConnection(sqlconnection))
+            {
+
+                using (SqlCommand sqlcmd = new SqlCommand(spname, con))
+                {
+                    sqlcmd.Connection = con;
+                    con.Open();
+                    sqlcmd.CommandType = CommandType.StoredProcedure;
+                    using (SqlDataAdapter sda = new SqlDataAdapter(sqlcmd))
+                    {
+                        sda.Fill(ds);
+                    }
+                    con.Close();
+                }
+            }
+            return ds;
+        }
+
+        /// <summary>
+        /// myobj - being the model of your object
+        /// spname - provide your stored procedure name
+        /// sqlconnection - provide your complete connection string 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="myobj"></param>
+        /// <param name="spname"></param>
+        /// <param name="sqlconnection"></param>
+        /// <returns>2 Table value will be returned, one being the recordcount and the other being the data</returns>
+        public static DataTable GetDataTable<T>(T myobj, string spname, string sqlconnection)
+        {
+            
+
+            DataTable ToBind = new DataTable();
+
+            using (SqlConnection con = new SqlConnection(sqlconnection))
+            {
+
+                using (SqlCommand sqlcmd = new SqlCommand(spname, con))
+                {
+                    var properties = typeof(T).GetProperties();
+                    foreach (var property in properties)
+                    {
+                        SqlParameter sqlparam = new SqlParameter("@" + property.Name, property.GetValue(myobj, null));
+                        sqlcmd.Parameters.Add(sqlparam);
+                    }
+                   
+                    con.Open();
+                    sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataReader myReturnObj = sqlcmd.ExecuteReader();
+                   
+                    int k = -1;
+
+                    for (int columns = 0; columns < myReturnObj.FieldCount; columns++)
+                    { ToBind.Columns.Add(myReturnObj.GetName(columns)); }
+
+                    int NoofColumns = ToBind.Columns.Count;
+
+                    while (myReturnObj.Read())
+                    {
+                        DataRow dr = ToBind.NewRow();
+                        for (int i = 0; i < NoofColumns; i++)
+                        {
+                            k = myReturnObj.GetOrdinal(ToBind.Columns[i].ColumnName);
+                            if (k > -1)
+                            {
+
+                                dr[k] = myReturnObj[ToBind.Columns[i].ColumnName];
+
+                            }
+                        }
+                        ToBind.Rows.Add(dr);
+                    }
+
+                    con.Close();
+
+                }
+            }
+
+            return ToBind;
 
         }
-        public void Update()
+
+        /// <summary>
+        /// spname - provide your stored procedure name
+        /// sqlconnection - provide your complete connection string 
+        /// </summary>
+        /// <param name="spname"></param>
+        /// <param name="sqlconnection"></param>
+        /// <returns>2 Table value will be returned, one being the recordcount and the other being the data</returns>
+        public static DataTable GetDataTable(string spname, string sqlconnection)
         {
 
-        }
-        public void Read()
-        {
+            DataTable ToBind = new DataTable();
+
+            using (SqlConnection con = new SqlConnection(sqlconnection))
+            {
+
+                using (SqlCommand sqlcmd = new SqlCommand(spname, con))
+                {
+        
+                   
+                    con.Open();
+                    sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataReader myReturnObj = sqlcmd.ExecuteReader();
+                    
+                    int k = -1;
+
+                    for (int columns = 0; columns < myReturnObj.FieldCount; columns++)
+                    { ToBind.Columns.Add(myReturnObj.GetName(columns)); }
+
+                    int NoofColumns = ToBind.Columns.Count;
+
+                    while (myReturnObj.Read())
+                    {
+                        DataRow dr = ToBind.NewRow();
+                        for (int i = 0; i < NoofColumns; i++)
+                        {
+                            k = myReturnObj.GetOrdinal(ToBind.Columns[i].ColumnName);
+                            if (k > -1)
+                            {
+
+                                dr[k] = myReturnObj[ToBind.Columns[i].ColumnName];
+
+                            }
+                        }
+                        ToBind.Rows.Add(dr);
+                    }
+
+                    con.Close();
+
+                }
+            }
+
+            return ToBind;
 
         }
-        public void Delete()
-        {
 
-        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,72 @@
 # MyCrudPackage
+Purpose
+-----------
+This package is useful to everyone who uses .net framework and try to push the model data to the sql database.
+
+Implementation Steps
+-----------------------
+we have 3 methods in this package
+
+1. DBTransaction	- Applicable to send model as a parameter and do the CUD operations
+2. GetDataSet		= Applicable to receive the dataset with multiple or single table
+3. GetDataTable		- Applicable to receive the datatable 
+
+Scenario 1:
+Assuming you want to do a transaction to create / update / delete a record
+Model (From your Application)
+------
+public class Employee
+{
+	public string FirstName{get;set;}
+	public string LastName{get;set;}
+	public string MiddleName{get;set;}
+	public int Age{get;set;}
+	public bit Gender{get;set;}
+}
+you are forming this object in your creation screen (Employee.aspx.cs)
+
+Button_Click event
+--------------------
+Employee employee=new Employee();
+employee.FirstName=txtFirstName.Text;
+employee.LastName=txtLastName.Text;
+employee.MiddleName=txtMiddleName.Text;
+employee.Age=Convert.ToInt16(txtAge.Text);
+employee.Gender=Convert.ToBoolean(rbdGender.Value);
+
+here we call our MyCrudPackage
+-------------------------------
+var result=MyCrudPackage.CRUD.DBTransactions<Employee>(employee,<Storedprocname>,<sqlconnectionstring>);
+
+Scenario 2:
+-----------
+To receive data to a Dataset with parameters (multiple collection of select statement from stored proc / single)
+
+DataSet ds=new DataSet();
+ds=MyCrudPackage.CRUD.DBTransactions<Employee>(employee,<Storedprocname>,<sqlconnectionstring>);
+
+Scenario 3:
+-----------
+To receive data to a Dataset without parameters (multiple collection of select statement from stored proc / single)
+
+DataSet ds=new DataSet();
+ds=MyCrudPackage.CRUD.DBTransactions(<Storedprocname>,<sqlconnectionstring>);
+
+Scenario 4:
+-----------
+To receive data to a Datatable with parameters
+
+DataTable dt=new DataTable();
+dt=MyCrudPackage.CRUD.DBTransactions<Employee>(employee,<Storedprocname>,<sqlconnectionstring>);
+
+Scenario 5:
+-----------
+To receive data to a Datatable without parameters 
+
+DataTable dt=new DataTable();
+dt=MyCrudPackage.CRUD.DBTransactions(<Storedprocname>,<sqlconnectionstring>);
+
+
+
+
+
